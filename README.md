ec574ef4acf1d0334045a596a07df3136d187a3d
Rosenstrauch <Rosenstrauch@2318810.no-reply.drupal.org>
2017-02-26 09:10:30 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+node red node for completing a habitica task
+
+https://habitica.com
+https://nodered.org/
+https://www.npmjs.com/package/habitica
---
+++ habitica/daily.js
@@ -0,0 +1,54 @@
+var Habitica = require('habitica');
+var api = new Habitica({
+
+})
+
+module.exports = function (RED) {
+    function completeDailyNode(config) {
+        RED.nodes.createNode(this, config);
+        var node = this;
+        this.userid = this.credentials.userid;
+        this.apikey = this.credentials.apikey;
+        this.taskid = config.taskid;
+
+        this.on('input', function (msg) {
+
+
+            api.setOptions({
+                id: this.userid,
+                apiToken: this.apikey
+            })
+
+
+            return api.post('/tasks/' + this.taskid + '/score/up').then((resolve) => {
+
+                console.log('tried to post to '+ this.taskid)
+
+            }).catch((err) => {
+                if (err instanceof Habitica.ApiError) {
+                // likely a validation error from
+                // the API request
+                console.log(err.message)
+            } else if (err instanceof Habitica.UnknownConnectionError) {
+                // either the Habitica API is down
+                // or there is no internet connection
+                console.log(err.originalError)
+            } else {
+                // there is something wrong with your integration
+                // such as a syntax error or other problem
+                console.log(err)
+            }
+        })
+
+
+
+    });
+    }
+
+    RED.nodes.registerType("complete-daily", completeDailyNode, {
+        credentials: {
+            userid: {type: "text"},
+            apikey: {type: "password"}
+        }
+    });
+}
